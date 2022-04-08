02700a470ffd40d576b48e30519060a20311dfa3
LUH <ung7497@ajou.ac.kr>
2022-04-08 23:01:19 +0900

    component; Add submit button

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Keyboard,
+} from "react-native";
 
 const AddTodo = () => {
+  const [text, setText] = React.useState("");
+  const onPress = () => {
+    setText("");
+    Keyboard.dismiss();
+  };
+  const button = (
+    <View style={styles.buttonStyle}>
+      <Image source={require("../assets/icons/add_white/add_white.png")} />
+    </View>
+  );
   return (
     <View style={styles.block}>
-      <TextInput placeholder="할 일을 입력하세요." style={styles.input} />
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        placeholder="할 일을 입력하세요."
+        style={styles.input}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+      <TouchableOpacity onPress={onPress} activeOpacity={0.5}>
+        {button}
+      </TouchableOpacity>
     </View>
   );
 };
@@ -17,10 +44,21 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderBottomWidth: 1,
     justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
   },
   input: {
     fontSize: 16,
     paddingVertical: 8,
+    flex: 1,
+  },
+  buttonStyle: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 48,
+    height: 48,
+    backgroundColor: "#d1c4e9",
+    borderRadius: 24,
   },
 });
 
